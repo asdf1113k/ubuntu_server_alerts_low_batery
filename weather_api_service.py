567d5482cff9bcef6e0daf8aaafd6ee15f08d7b1
Maxim Bronnykov(TypeErr0r) <asdf1113k.gmail.com>
2026-03-07 13:49:03 +0300

    было: получение json в weather.py стало: json выгружаеться в словарь в самом(а не в weather.py)  weather_api_service.py и импортируеться в weather.py

--- weather_api_service.py
+++ weather_api_service.py
@@ -4,7 +4,7 @@ from my_token import TOKEN
 # https://api.openweathermap.org/data/2.5/weather?lat=55.7&lon=37.5&appid=7549b3ff11a7b2f3cd25b56d21c83c6a&lang=ru&units=metric
 loc = json_ipinfo["loc"].strip(",")
 responce_api_openweather = rq.get(f"https://api.openweathermap.org/data/2.5/weather?lat={loc[0]}&lon={loc[1]}&appid={TOKEN}&lang=ru&units=metric")
-
+json_openweather = responce_api_openweather.json()
 
 if __name__ == "__main__":
     print(responce_api_openweather.status_code)
