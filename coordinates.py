b7925d4d92c3f9195a63f6cf853f63749902b02c
Maxim Bronnykov(TypeErr0r) <asdf1113k.gmail.com>
2026-03-07 16:25:37 +0300

    перенёс строку кода скрывающую ip с точки входа в файл

--- coordinates.py
+++ coordinates.py
@@ -2,8 +2,8 @@ import requests as rq
 
 responce_ipinfo = rq.get("https://ipinfo.io")
 json_ipinfo = responce_ipinfo.json()
+json_ipinfo["ip"] = "hidden"
 
 if __name__ == "__main__":
-    json_ipinfo["ip"] = "hidden"
     for key in json_ipinfo:
         print(key, ":", json_ipinfo[key])
