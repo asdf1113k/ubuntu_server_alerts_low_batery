76045a7d5baedf44842e854232bfcb8e1a7e5fcc
Maxim Bronnykov(TypeErr0r) <asdf1113k.gmail.com>
2026-03-07 13:34:44 +0300

    убран лишний символ

--- test.py
+++
@@ -1,9 +0,0 @@
-# import requests as rq
-# from coordinates import json_ipinfo
-
-
-# responce = rq.get(f"https://api.openweathermap.org/data/3.0/onecall?lat={json_ipinfo["lat"]}&lon={lon}&exclude={part}&appid={API key}")
-
-import subprocess
-
-subprocess.run("curl https://api.openweathermap.org/data/2.5/weather\?lat\=55.7\&lon\=37.5\&appid\=7549b3ff11a7b2f3cd25b56d21c83c6a\&lang\=ru\&units\=metric")
