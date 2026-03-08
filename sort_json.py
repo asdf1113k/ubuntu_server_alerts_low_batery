0bac71eb06d4361e9ca0b676adcf02b705a52b72
Maxim Bronnykov(TypeErr0r) <asdf1113k.gmail.com>
2026-03-08 15:36:43 +0300

    добавлен print

--- sort_json.py
+++ sort_json.py
@@ -2,6 +2,7 @@ from coordinates import json_ipinfo
 from weather_api_service import json_openweathermap
 if __name__ == "__main__":
     print(json_ipinfo)
+    print()
     print(json_openweathermap)
 
 
