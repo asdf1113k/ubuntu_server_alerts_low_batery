eeda2492ded474ce4f0df54933f95b3de73df17b
Maxim Bronnykov(TypeErr0r) <asdf1113k.gmail.com>
2026-03-10 16:27:46 +0300

    функция filter_color обнавлена

--- filter_color.py
+++ filter_color.py
@@ -1,7 +1,9 @@
 from colorama import init, Fore
 init(autoreset=True)
 
-def filter_color(obj: int | float | str):
+def filter_color_old_version(obj: int | float | str) -> str:
+     """это версия функции больше не используеться, остаеться здесь как экспанат в музее
+     ну или если у когото в друг будет версия python>3.10"""
      if isinstance(obj, str):
           if obj in ['ясно']:
                return Fore.YELLOW + obj
@@ -20,6 +22,23 @@ def filter_color(obj: int | float | str):
           else:
                return obj
           
-def filter_color2(obj):
-     ...
+def filter_color(obj: str | int | float) -> str:
+     match obj:
+          case 'ясно':
+               return Fore.YELLOw + obj
+
+          case 'переменная облачность' | 'облачно':
+               return Fore.WHITE + obj
+          
+          case obj if obj < 0:
+               return Fore.BLUE + str(obj)
+          
+          case obj if obj > 10:
+               return Fore.YELLOW + str(obj)
+
+          case obj if obj <= 10:
+               return Fore.CYAN + str(obj)
+          
+          case _:
+               return obj
      
