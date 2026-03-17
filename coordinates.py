51587887409eea4cf3f4a9377b6dbf04f59899d0
Maxim Bronnykov(TypeErr0r) <asdf1113k@gmail.com>
2026-03-17 09:58:20 +0300

    coordinates.py переписываеться на версию с более точным получением координат

--- coordinates.py
+++ coordinates.py
@@ -1,9 +1,1 @@
-import requests as rq
 
-responce_ipinfo = rq.get("https://ipinfo.io")
-json_ipinfo = responce_ipinfo.json()
-json_ipinfo["ip"] = "hidden"
-
-if __name__ == "__main__":
-    for key in json_ipinfo:
-        print(key, ":", json_ipinfo[key])
