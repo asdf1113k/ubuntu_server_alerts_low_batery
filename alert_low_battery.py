000fa177e22e45f5187524bb0b921224fcdc7775
Maxim Bronnykov(TypeErr0r) <asdf1113k@gmail.com>
2026-03-24 13:03:30 +0300

    примерно накидал функционал

--- alert_low_battery.py
+++ alert_low_battery.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 import subprocess
+import time
 
-result = subprocess.run(['cat', '/sys/class/power_supply/BAT1/capacity'], capture_output=True, text=True)
 
-battery_charge:int = int(result.stdout)
+def sound_low_battery():
+    subprocess.run(['say', '"low battery"'])
+
+# get battery charge and connected charging(запрос заряда батареи и подключена ли зарядка)
+result_capacity = subprocess.run(['cat', '/sys/class/power_supply/BAT1/capacity'], capture_output=True, text=True)
+result_online = subprocess.run(['cat', '/sys/class/power_supply/AC1/online'], capture_output=True, text=True)
+battery_charge:     int = int(result_capacity.stdout)
+connected_charging: int = int(result_online.stdout)
 print(battery_charge)
+
+while 1 == 1:
+    if battery_charge <= 20 and 0 == connected_charging:
+        sound_low_battery()
+        time.sleep(2)
+    else:
+        time.sleep(10)
