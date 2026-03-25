99e3b1f86f66427af500ae37726b94bbfc0ffd85
Maxim Bronnykov(TypeErr0r) <asdf1113k@gmail.com>
2026-03-25 15:14:44 +0300

    add: отлов исключения KeyBoardInterrupt

--- AlertLowBattery.py
+++ AlertLowBattery.py
@@ -11,17 +11,19 @@ name_dir_info_battery = name_dir_info_battery.stdout.split()
 def sound_low_battery():
     subprocess.run(['say', '"low battery"'])
 
+try:
+    while 1 == 1:
+        # get battery charge and connected charging(запрос заряда батареи и подключена ли зарядка)
+        result_capacity = subprocess.run(['cat', f'/sys/class/power_supply/{name_dir_info_battery[1]}/capacity'], capture_output=True, text=True)
+        result_online = subprocess.run(['cat', f'/sys/class/power_supply/{name_dir_info_battery[0]}/online'], capture_output=True, text=True)
+        battery_charge:     int = int(result_capacity.stdout)
+        connected_charging: int = int(result_online.stdout)
 
-while 1 == 1:
-    # get battery charge and connected charging(запрос заряда батареи и подключена ли зарядка)
-    result_capacity = subprocess.run(['cat', f'/sys/class/power_supply/{name_dir_info_battery[1]}/capacity'], capture_output=True, text=True)
-    result_online = subprocess.run(['cat', f'/sys/class/power_supply/{name_dir_info_battery[0]}/online'], capture_output=True, text=True)
-    battery_charge:     int = int(result_capacity.stdout)
-    connected_charging: int = int(result_online.stdout)
-
-    if battery_charge <= 20 and 0 == connected_charging:
-        sound_low_battery()
-        time.sleep(2)
-    else:
-        print(f'{battery_charge}% статус зарядного устройства: {connected_charging}')
-        time.sleep(4)
+        if battery_charge <= 20 and 0 == connected_charging:
+            sound_low_battery()
+            time.sleep(2)
+        else:
+            print(f'{battery_charge}% статус зарядного устройства: {connected_charging}')
+            time.sleep(4)
+except KeyboardInterrupt:
+    pass
