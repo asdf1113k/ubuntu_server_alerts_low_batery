054bd770a2454415ab32cbf5a21f5c5a279e2b70
Maxim Bronnykov(TypeErr0r) <asdf1113k@gmail.com>
2026-03-25 15:05:45 +0300

    test auto определения названия папки с данными о батареи

--- AlertLowBattery.py
+++ AlertLowBattery.py
@@ -3,14 +3,20 @@ import subprocess
 import time
 
 
+name_dir_info_battery = subprocess.run(['ls', '/sys/class/power_supply/'], capture_output=True, text=True)
+name_dir_info_battery = name_dir_info_battery.stdout.split().sort()
+print(name_dir_info_battery)
+
+
+
 def sound_low_battery():
     subprocess.run(['say', '"low battery"'])
 
 
 while 1 == 1:
     # get battery charge and connected charging(запрос заряда батареи и подключена ли зарядка)
-    result_capacity = subprocess.run(['cat', '/sys/class/power_supply/BAT1/capacity'], capture_output=True, text=True)
-    result_online = subprocess.run(['cat', '/sys/class/power_supply/AC1/online'], capture_output=True, text=True)
+    result_capacity = subprocess.run(['cat', f'/sys/class/power_supply/{name_dir_info_battery[1]}/capacity'], capture_output=True, text=True)
+    result_online = subprocess.run(['cat', f'/sys/class/power_supply/{name_dir_info_battery[0]}/online'], capture_output=True, text=True)
     battery_charge:     int = int(result_capacity.stdout)
     connected_charging: int = int(result_online.stdout)
 
