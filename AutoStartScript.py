4d2cd8c88407f8ab7d448f9b140e092ae0eaa74a
Maxim Bronnykov(TypeErr0r) <asdf1113k@gmail.com>
2026-03-26 14:52:18 +0300

    added in file AutoStartScript.py comments

--- AutoStartScript.py
+++ AutoStartScript.py
@@ -1,8 +1,11 @@
 import subprocess 
-# получение имени текущенго  пользователя
+# получение имени текущенго  пользователя и пути к директории
 pwd = subprocess.run(['pwd'], capture_output=True, text=True).stdout
 user = pwd.split('/')[2]
 print(user)
+
+print('установка зависимостей...')
+subprocess.run(['sudo', 'apt', 'install', 'paplay',])
 
 with open('AlertLowBattery.service', 'w') as file_service:
     file_service.write(
@@ -20,9 +23,9 @@ WorkingDirectory={pwd}
 WantedBy=multi-user.target
 """
 )
-
+# копирование AlertLowBattery.service в /etc/systemd/system
 subprocess.run(['sudo', 'cp', 'AlertLowBattery.service', '/etc/systemd/system/AlertLowBattery.service',])
-
+# перезагрузка демона и настройка авто запуска скрипта
 subprocess.run(['sudo', 'systemctl', 'daemon-reload',])
 subprocess.run(['sudo', 'systemctl', 'enable', 'AlertLowBattery.service',])
 subprocess.run(['sudo', 'systemctl', 'start', 'AlertLowBattery.service',])
