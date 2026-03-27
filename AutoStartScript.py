741ebd04723c8aa01d111fb3017b8ac3ec913178
Maxim Bronnykov(TypeErr0r) <asdf1113k@gmail.com>
2026-03-27 15:31:53 +0300

    ed: AutoStartScript.py исправлена ошибка с типом CompletedProcess исправлен путь создания каталока для *.service

--- AutoStartScript.py
+++ AutoStartScript.py
@@ -8,11 +8,11 @@ subprocess.run(['sudo', 'apt', 'install', 'paplay',])
 # получение имени текущенго пользователя и пути к директории
 PATH_TO_SRCIPT = Path(__file__)
 PATH_TO_FOLDER_WITH_SRCIPT = PATH_TO_SRCIPT.parent
-USER = subprocess.run(['whoami'], capture_output=True, text=True)
-print(USER)
+USER = subprocess.run(['whoami'], capture_output=True, text=True).stdout
+print(USER[:-1])
 
 # создание AlertLowBattery.service в /etc/systemd/system
-subprocess.run(['mkdir', '-p', '~/.config/systemd/user/',])
+subprocess.run(['mkdir', '-p', f'/home/{USER[:-1]}/.config/systemd/user/',])
 with open('~/.config/systemd/user/AlertLowBattery.service', 'w') as file_service:
     file_service.write(
 f"""
@@ -23,7 +23,7 @@ After=sound.target pipewire.service pipewire-pulse.service wireplumber.service
 [Service]
 ExecStart=/usr/bin/python3 {PATH_TO_SRCIPT}
 Type=simple
-User={USER}
+User={USER[:-1]}
 WorkingDirectory={PATH_TO_FOLDER_WITH_SRCIPT}
 
 [Install]
