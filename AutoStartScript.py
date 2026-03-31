ab8a16c81f48a490200325852610f5336b5f6ddd
Maxim Bronnykov(TypeErr0r) <asdf1113k@gmail.com>
2026-03-31 15:29:28 +0300

    BigCommit: откройте для подробного просмотра add: description script remove: [16] print from [18] print remove: comments с права строки кода fixed: [44] 'USERNAME' on 'user' fixed: [48] typo 'ssystemctl' replace 'systemctl'

--- AutoStartScript.py
+++ AutoStartScript.py
@@ -1,25 +1,27 @@
+"""
+скрипт который сам добавит AlertLowBattery.py
+в автозапуск systemctl --user
+"""
+
 from pathlib import Path
 import os
 import getpass
 
-# получение имени текущенго пользователя и пути к директории
 path_to_script = Path(__file__)
 path_to_script = path_to_script.parent
-PATH_TO_FOLDER_WITH_SRCIPT = path_to_script
-path_to_script = path_to_script / 'AlertLowBattery.py'
+PATH_TO_FOLDER_WITH_SRCIPT = path_to_script # получение пути к директории
+path_to_script = path_to_script / 'AlertLowBattery.py' # получение пути к файлу AlertLowBattery.py
 
-USERNAME = getpass.getuser()
-print(USERNAME)
+USERNAME = getpass.getuser() # получение имени пользователя
 
 if __name__ == "__main__":
+    print(f'пользователь: {USERNAME}')
     print('установка зависимостей...')
     os.system('sudo apt install pulseaudio-utils')
     os.system('sudo apt install beep')
 
-    # создание AlertLowBattery.service в ~/.config/systemd/user
-    os.system(f'cd {PATH_TO_FOLDER_WITH_SRCIPT}')
-    os.system(f'mkdir -p /home/{USERNAME}/.config/systemd/user/')
-    os.system(f'sudo chown {USERNAME}:{USERNAME} /home/{USERNAME}/.config/systemd/user') # ДАЮ ПРАВА ПОЛЬЗОВАТЕЛЮ
+    os.system(f'mkdir -p /home/{USERNAME}/.config/systemd/user/') # создание папок '~/.config/systemd/user/'
+    os.system(f'sudo chown {USERNAME}:{USERNAME} /home/{USERNAME}/.config/systemd/user') # выдача прав доступа к каталогу
     with open('AlertLowBattery.service', 'w') as file_service:
         file_service.write(
     f"""
@@ -39,14 +41,12 @@ WantedBy=default.target
 """
 )
         file_service.close()
-    # копирование AlertLowBattery.service в ~/.config/systemd/USERNAME/
-    os.system(f'cp -v /{PATH_TO_FOLDER_WITH_SRCIPT / 'AlertLowBattery.service'} /home/{USERNAME}/.config/systemd/user/'   )
+    # копирование AlertLowBattery.service в ~/.config/systemd/user/
+    os.system(f'cp -v /{PATH_TO_FOLDER_WITH_SRCIPT / 'AlertLowBattery.service'} /home/{USERNAME}/.config/systemd/user/')
         
-
-    # перезагрузка демона и настройка авто запуска скрипта
-    os.system('systemctl --user daemon-reload')
-    os.system('ssystemctl --user enable --now AlertLowBattery.service')
-    os.system('systemctl --user start AlertLowBattery.service')
-    os.system('systemctl --user status AlertLowBattery.service')
+    os.system('systemctl --user daemon-reload') # перезагрузка демона
+    os.system('systemctl --user enable --now AlertLowBattery.service') # настройка авто запуска скрипта
+    os.system('systemctl --user start AlertLowBattery.service') # запуск скрипта в фоновом режиме
+    os.system('systemctl --user status AlertLowBattery.service') # проверка статуса скрипта
 
     print(f"путь до скрипта {path_to_script}")
